4ad744713d38ca3ba9f62480e73f9af51ccb9c08
rjdeleon <ricardopablo.deleon@gmail.com>
2018-03-31 05:17:02 +0800

    Add stub for handling CORS issues

--- app.js
+++ app.js
@@ -18,6 +18,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// CORS setup
+// app.use(function(req, res, next) {  
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+//   next();
+// });
+
 app.use('/', appRouter);
 
 // catch 404 and forward to error handler
