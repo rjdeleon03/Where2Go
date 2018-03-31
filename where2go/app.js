6bb8b6ce3aaa89820e93ce808cc7ce29a661ee55
DESKTOP-5FEF2FS <ricardopablo.deleon@gmail.com>
2018-03-31 15:49:04 +0800

    Configure project for angular build then serve with node

--- where2go/app.js
+++ where2go/app.js
@@ -1,8 +1,28 @@
-var express = require("express");
+var express     = require("express");
+var path        = require("path");
+var favicon     = require("serve-favicon");
+var logger      = require("morgan");
+var bodyParser  = require("body-parser");
+
+// var bookRoute = require("./routes/book");
 var app = express();
 
-app.get("/", function(req, res) {
-    res.send("SERVER ROOT");
-});
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({"extended": "false"}));
+app.use(express.static(path.join(__dirname, "dist")));
 
-app.listen(3000);
+// app.use("/book", bookRoute);
+
+// error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
+  
+module.exports = app;
