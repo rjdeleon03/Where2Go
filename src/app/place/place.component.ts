17849b67acf03b5c258f7eef019bb72f12999bab
DESKTOP-5FEF2FS <ricardopablo.deleon@gmail.com>
2018-04-03 20:33:46 +0800

    Add more home page styling

--- src/app/place/place.component.ts
+++ src/app/place/place.component.ts
@@ -3,6 +3,8 @@ import { HttpClient } from '@angular/common/http';
 import { PlacesService } from '../services/places.service';
 import { Place } from '../../models/place';
 
+// import * as $ from 'jquery';
+
 @Component({
   selector: 'app-place',
   templateUrl: './place.component.html',
@@ -16,7 +18,32 @@ export class PlaceComponent implements OnInit {
   ngOnInit() {    
     this.placeService.getPlaces().subscribe((places: Place[]) => {
       this.places = places;
-      console.log(places);
-    })
+    });
+
+    /*
+    // jQuery
+    $(document).ready(function() {    
+
+      $(".place-div").each(function() {
+        $(this).css("opacity", "0");
+        if ($(this).offset().top < $(window).scrollTop() + $(window).innerHeight()) {
+          $(this).animate({              
+            opacity: 1.0
+          }, 500);
+        }
+      });
+
+      $(window).scroll(function() {
+        $(".place-div").each(function() {
+          if ($(this).offset().top >= $(window).scrollTop() + $(window).innerHeight()) {
+            console.log($(this).offset().top + " --- " + ($(window).scrollTop() + $(window).innerHeight()));
+            $(this).animate({              
+              opacity: 1.0
+            }, 500);
+          }   
+        })
+      });
+    });
+    */
   }
 }
